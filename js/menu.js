e07ebd4079cabcee7d050408a7adcab60f65b22a
Dan <danielsuarez.laboral@gmail.com>
2016-10-18 13:31:37 -0300

    Iniciamos quie se encuentran todos los archivos basicos de inicio

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+// var menuBar=document.getElementById('menu-bar');
+// var menu=document.getElementById('menu');
+
+// menuBar.addEventListener('click',function(){
+// 	menu.classList.toggle('mostrar');
+// });
---
+++ js/script.js
@@ -0,0 +1,7 @@
+// $(window).scroll(function(){
+//     if ($(this).scrollTop() > 0) {
+//         $('.btn_top').fadeIn();
+//     } else {
+//         $('.btn_top').fadeOut();
+//     }
+// });
